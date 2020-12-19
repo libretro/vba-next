a4badce3176a33e3cd2bb5b6d5c04c3d96a01953
twinaphex <libretro@gmail.com>
2020-12-19 16:01:11 +0100

    Update

--- libretro-common/include/retro_miscellaneous.h
+++ libretro-common/include/retro_miscellaneous.h
@@ -26,9 +26,7 @@
 #include <stdint.h>
 #include <math.h>
 
-#if defined(__CELLOS_LV2__) && !defined(__PSL1GHT__)
-#include <sys/timer.h>
-#elif defined(XENON)
+#if defined(XENON)
 #include <time/time.h>
 #elif defined(GEKKO) || defined(__PSL1GHT__) || defined(__QNX__)
 #include <unistd.h>
@@ -87,9 +85,7 @@
  **/
 static INLINE void retro_sleep(unsigned msec)
 {
-#if defined(__CELLOS_LV2__) && !defined(__PSL1GHT__)
-   sys_timer_usleep(1000 * msec);
-#elif defined(PSP) || defined(VITA)
+#if defined(PSP) || defined(VITA)
    sceKernelDelayThread(1000 * msec);
 #elif defined(_3DS)
    svcSleepThread(1000000 * (s64)msec);
--- libretro-common/rthreads/rthreads.c
+++ libretro-common/rthreads/rthreads.c
@@ -43,9 +43,6 @@
 #include "gx_pthread.h"
 #elif defined(PSP) || defined(VITA)
 #include "psp_pthread.h"
-#elif defined(__CELLOS_LV2__)
-#include <pthread.h>
-#include <sys/sys_time.h>
 #else
 #include <pthread.h>
 #include <time.h>
@@ -442,13 +439,6 @@ bool scond_wait_timeout(scond_t *cond, slock_t *lock, int64_t timeout_us)
    mach_port_deallocate(mach_task_self(), cclock);
    now.tv_sec = mts.tv_sec;
    now.tv_nsec = mts.tv_nsec;
-#elif defined(__CELLOS_LV2__)
-   sys_time_sec_t s;
-   sys_time_nsec_t n;
-
-   sys_time_get_current_time(&s, &n);
-   now.tv_sec  = s;
-   now.tv_nsec = n;
 #elif defined(__mips__) || defined(VITA)
    struct timeval tm;
 
